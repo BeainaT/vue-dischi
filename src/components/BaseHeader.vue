f7c0ad6c149d5f103e26302a5b9a1060ca61bed2
BeainaT <tania.df@hotmail.it>
2022-06-07 15:22:19 +0200

    added header

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <div class="container-fluid py-2">
+        <HeaderLogo/>
+      </div>
+  </header>
+</template>
+
+<script>
+import HeaderLogo from './HeaderComponents/HeaderLogo.vue';
+
+export default {
+    name: "BaseHeader",
+    components: { 
+        HeaderLogo 
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        background-color: var(--primary_bg_color);
+    }
+</style>
---
+++ src/components/HeaderComponents/HeaderLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <img src="../../assets/img/spotify-logo.png" alt="logo">
+</template>
+
+<script>
+export default {
+    name: 'HeaderLogo'
+}
+</script>
+
+<style lang="scss" scoped>
+
+    img {
+        width: 5rem;
+    }
+
+</style>

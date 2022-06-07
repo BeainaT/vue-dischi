531869c1b0cc03c84c77aed599fa676f44e9de97
BeainaT <tania.df@hotmail.it>
2022-06-07 17:57:57 +0200

    added bonus

--- src/components/MainComponents/AlbumSection.vue
+++ src/components/MainComponents/AlbumSection.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
-      <div class="container py-5">
+      <LoaderSpin/>
+      <div class="container py-5 v-else">
           <div class="row gap-4 justify-content-center">
               <AlbumComponent class="col-sm-12 col-md-4 col-xl-2" v-for="(album, index) in albums" :key="index" :album="album"/>
           </div>
@@ -11,22 +12,26 @@
 <script>
 import axios from 'axios';
 import AlbumComponent from './AlbumComponent.vue';
+import LoaderSpin from './LoaderSpin.vue';
+
 
 export default {
     name: 'AlbumSection',
     components: {
         AlbumComponent,
+        LoaderSpin
     },
     data() {
         return {
             albums: [],
+            loading: true,
         }
     },
     created() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then((response) => {
             this.albums = response.data.response;
-            console.log(this.albums)
+            this.loading = false;
         })
         .catch((error) => {
             console.log(error);
---
+++ src/components/MainComponents/LoaderSpin.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="box_animation container">
+        <div class="loader_spin" v-if="loading"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoaderSpin'
+}
+</script>
+
+<style lang="scss" scoped>
+    .box_animation {
+        padding: 3.125rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .loader_spin {
+            border: 1.25rem solid var(--text_primary);
+            border-top: 1.25rem solid var(--primary_bg_color); /* Blue */
+            border-radius: 50%;
+            width: 12.5rem;
+            height: 12.5rem;
+            animation: spin 2s linear infinite;
+        }
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+
+</style>
